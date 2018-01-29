ac307a8820e7ee8f766055c6223ba215e706d831
aditya-kumbhar <kaditya1802@gmail.com>
2018-01-29 19:10:36 +0530

    JSON request handled

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -8,88 +8,92 @@ import json
 def index():
     return render_template("index.html")
 
-@app.route("/signup", methods = ['POST'])
-def signup():
-    print(request.get_json())
-    return 'OK'
-    
-# @app.route("/signup", methods = ['GET','POST'])
+# @app.route("/signup", methods = ['POST'])
 # def signup():
-#     url = "https://auth.asthmatic70.hasura-app.io/v1/signup"
-#     name = request.form['name']
-#     username = request.form['username']
-#     password = request.form['pass']
-#     email = request.form['email']
-#     mobile = request.form['mobile']
-#     city = request.form['city']
-#     birthdate = request.form['birthdate']
+#     result = request.get_json(force=True)
+
+#     print(result['device'])
+#     return 'OK'
     
-#     # This is the json payload for the query
-#     requestPayload = {
-#         "provider": "username",
-#         "data": {
-#             "username": username,
-#             "password": password
-#         }
-#     }
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
+    url = "https://auth.asthmatic70.hasura-app.io/v1/signup"
+    
+    user_info = request.get_json(force=True)
+    name = user_info['name']
+    username = user_info['username']
+    password = user_info['pass']
+    email = user_info['email']
+    mobile = user_info['mobile']
+    city = user_info['city']
+    birthdate = user_info['birthdate']
+    
+    # This is the json payload for the query
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": username,
+            "password": password
+        }
+    }
 
-#     # Setting headers
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
 
-#     # Make the query and store response in resp
-#     resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-#     resp = json.loads(resp.content)     # resp.content contains the json response.
-#     if 'hasura_id' in resp.keys():
-#         #signup success. insert data in users table
-#         # This is the url to which the query is made
-#         url = "https://data.asthmatic70.hasura-app.io/v1/query"
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    resp = json.loads(resp.content)     # resp.content contains the json response.
+    if 'hasura_id' in resp.keys():
+        #signup success. insert data in users table
+        # This is the url to which the query is made
+        url = "https://data.asthmatic70.hasura-app.io/v1/query"
         
-#         hasura_id = resp['hasura_id']
-#         # This is the json payload for the query
-#         requestPayload = {
-#             "type": "insert",
-#             "args": {
-#                 "table": "users",
-#                 "objects": [
-#                     {
-#                         "name"  :name,
-#                         "birthdate" :birthdate ,
-#                         "hasura_id" :hasura_id ,
-#                         "username" :username ,
-#                         "password" :password ,
-#                         "email" :email ,
-#                         "mobile" :mobile ,
-#                         "city" :city
-#                         }
-#                 ]
-#             }
-#         }
+        hasura_id = resp['hasura_id']
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "insert",
+            "args": {
+                "table": "users",
+                "objects": [
+                    {
+                        "name"  :name,
+                        "birthdate" :birthdate ,
+                        "hasura_id" :hasura_id ,
+                        "username" :username ,
+                        "password" :password ,
+                        "email" :email ,
+                        "mobile" :mobile ,
+                        "city" :city
+                        }
+                ]
+            }
+        }
 
-#         # Setting headers
-#         headers = {
-#         "Content-Type": "application/json",
-#         "Authorization": "Bearer 8fed092aefc106fb907c6061de133b15a1e95c6310ec79c2"
-#         }
+        # Setting headers
+        headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 8fed092aefc106fb907c6061de133b15a1e95c6310ec79c2"
+        }
 
-#         # Make the query and store response in resp
-#         queryResp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+        # Make the query and store response in resp
+        queryResp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
 
-#         # resp.content contains the json response.
-#         print(queryResp.content)
-#         zapPostURL = 'https://hooks.zapier.com/hooks/catch/2907826/8vw21h/'
-#         zapPayLoad =  {
-#                         "name" :name,
-#                         "birthdate" :birthdate ,
-#                         "username" :username ,
-#                         "email" :email ,
-#                         "mobile" :mobile ,
-#                         "city" :city
-#                         }
-#         zapResp = requests.request("POST",zapPostURL, data = json.dumps(zapPayLoad))
-#         return 'OK'
-#     else:
-#         ans = resp['code']+" "+resp['message']  
-#         return ans
+        # resp.content contains the json response.
+        print(queryResp.content)
+        zapPostURL = 'https://hooks.zapier.com/hooks/catch/2907826/8vw21h/'
+        zapPayLoad =  {
+                        "name" :name,
+                        "birthdate" :birthdate ,
+                        "username" :username ,
+                        "email" :email ,
+                        "mobile" :mobile ,
+                        "city" :city
+                        }
+        zapResp = requests.request("POST",zapPostURL, data = json.dumps(zapPayLoad))
+        return 'Signup Successful.'
+    else:
+        ans = resp['code']+" "+resp['message']  
+        return ans
     
