70ace1edc358e04c31b1c8bf4d531abbcfb3da30
aditya-kumbhar <kaditya1802@gmail.com>
2018-01-31 22:07:21 +0530

    intergrating ui

---
+++ microservices/ui/app/src/components/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Layout.css";
+import Home from "./sections/home/Home";
+import Article from "./sections/Article";
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className="page__home">
+        <Home />
+        <Article />
+      </div>
+    );
+  }
+}
+
+export default Layout;
