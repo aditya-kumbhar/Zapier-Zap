e6cbfb0eacf5803c3393b9a8ee5efa64ad6536e4
aditya-kumbhar <kaditya1802@gmail.com>
2018-01-31 20:48:08 +0530

    new react ui ms added

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
-import Layout from "./components/Layout";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/normalize.css";
-import "./index.css";
-import App from "./App";
-import axios from "axios";
-import registerServiceWorker from "./registerServiceWorker";
-// https://api.belligerently39.hasura-app.io
-axios.defaults.baseURL = "https://ui.asthmatic70.hasura-app.io/";
-axios.defaults.headers.post["Content-Type"] = "application/json";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
