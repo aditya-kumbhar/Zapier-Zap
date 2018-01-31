33a67ea4ca6cf36af2278bcb3b1cb8ca28f80a45
aditya-kumbhar <kaditya1802@gmail.com>
2018-01-31 19:43:54 +0530

    Trying to integrate

--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -5,9 +5,9 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 import registerServiceWorker from "./registerServiceWorker";
-
-axios.defaults.baseURL = "https://api.belligerently39.hasura-app.io";
+// https://api.belligerently39.hasura-app.io
+axios.defaults.baseURL = "https://ui.asthmatic70.hasura-app.io/";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
 registerServiceWorker();
