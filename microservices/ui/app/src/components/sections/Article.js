acd00e61089ec44adec7f32374ab74e5f91beab9
Rahul Suresh <rahul.tech.work@gmail.com>
2018-02-23 17:03:13 +0530

    Changed name.

--- microservices/ui/app/src/components/sections/Article.js
+++ microservices/ui/app/src/components/sections/Article.js
@@ -5,7 +5,7 @@ import "./Article.css";
 const Article = props => {
   return (
     <div className="page__section page__section__article" id="login">
-      <h4 className="page__section__article--title">Sign up | T81PF</h4>
+      <h4 className="page__section__article--title">Sign up | Zapier - Gmail and Google Sheet Integration </h4>
 
       <Signup />
     </div>
